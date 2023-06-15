59bc3fdfd6b8bcfbf43937458f5f3c9170da6298
Fasholadiin <136710861+Fasholadiin@users.noreply.github.com>
2023-06-15 22:28:53 +0700

    Add files via upload

---
+++ latihan.bat
@@ -0,0 +1,21 @@
+@echo off
+title Latihan !
+rem =====Variabel======
+set P=50
+set L=60
+
+rem ======tampilan
+echo -------------------------------------
+echo Nim 		:20122068
+echo Nama 		:Muhammad Fasholadiin
+echo Kelas 		:TI 2B
+echo Jurusan		:Teknik Informatika
+echo -------------------------------------
+rem ====== rumus =========
+echo Diketahui
+set /a V= P*L
+echo panjang		:%P%
+echo lebar		:%L%
+echo -------------------------------------
+echo Luas bangunan= %V%
+pause
